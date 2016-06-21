26522c6ba97667d4a735bcdafa6271074831b8ca
KangarooMan64 <smithgar002@gmail.com>
2016-06-21 10:22:47 -0700

    pseudocode template

--- src/org/usfirst/frc/team3238/robot/Configuration.java
+++ src/org/usfirst/frc/team3238/robot/Configuration.java
@@ -8,11 +8,21 @@ package org.usfirst.frc.team3238.robot;
  import edu.wpi.first.wpilibj.IterativeRobot;
 public class Configuration {
 	public class Constants{
+		
 		int DriveMotorLeftFrontDeviceID = 0;
+		
+		public class Joystick {
+			int[] collectorInButton = new int[]{0, 1}; 
+//used by other class with Configuration.Constants.Joystick.collectorInButton[Configuration.Constants.Joystick.activePreset];
+			static int activePreset = 0;
+		}
 	}
 	public class misc{
 		CANTalon DriveMotorLeftFront = new CANTalon(Configuration.Constants.DriveMotorLeftFrontDeviceID);
-		
+		Chassis chassis = new Chassis(DriveMotorLeftFront, DriveMotorRightFront, ...);
 		//Shooter shooter = 
 	} 
+	public void ChangeJoystickMapping(int preset) {
+		Configuration.Constants.Joystick.activePreset = preset;
+	}
 }
