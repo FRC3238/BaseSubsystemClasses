1ae36622e993c92d3a3b69914c94a1003d7ee533
KangarooMan64 <smithgar002@gmail.com>
2016-06-21 10:02:35 -0700

    Update RobotWhisperer.java

--- src/org/usfirst/frc/team3238/robot/RobotWhisperer.java
+++ src/org/usfirst/frc/team3238/robot/RobotWhisperer.java
@@ -4,41 +4,64 @@ package org.usfirst.frc.team3238.robot;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-
+import java.util.*;
 public class RobotWhisperer extends JFrame {
 
+	public static final int FRAME_HEIGHT = 100, FRAME_WIDTH = 325;
+	JTextField[] identifierValues;
 	JTextField jtfText1, jtfUneditableText;
 	String disp = "";
 	TextHandler handler = null;
+	ArrayList<String> names;
+	ArrayList<Object> values;
 	
-	public RobotWhisperer() {
+	public static boolean update = false;
+	
+	public RobotWhisperer(ArrayList<String> names, ArrayList<Object> values) {
 		super("Robot Value Reader/Writer");
+
+		this.names = names;
+		this.values = values;
+		
+		identifierValues = new JTextField[names.size()];
+		
 		Container container = getContentPane();
 		container.setLayout(new FlowLayout());
-		jtfText1 = new JTextField(10);
-		jtfUneditableText = new JTextField("Uneditable text field", 20);
-		jtfUneditableText.setEditable(false);
-		container.add(jtfText1);
-		container.add(jtfUneditableText);
+
 		handler = new TextHandler();
-		jtfText1.addActionListener(handler);
-		jtfUneditableText.addActionListener(handler);
-		setSize(325, 100);
+		
+		for(int i = 0; i < identifierValues.length; i++) 
+			identifierValues[i] = new JTextField(names.get(i) + ": " + values.get(i), 10);
+			
+		for(JTextField j : identifierValues) {
+			container.add(j);
+			j.addActionListener(handler);
+		}
+		
+		setSize(FRAME_WIDTH, FRAME_HEIGHT);
 		setVisible(true);
 	}
 	
+	public boolean hasUpdate() {
+		return update;
+	}
+	public Object getUpdate(String id) {
+		return values.get(values.indexOf(id));
+	}
 	private class TextHandler implements ActionListener {
 
 		public void actionPerformed(ActionEvent e) {
-			if (e.getSource() == jtfText1) {
-				disp = "text1 : " + e.getActionCommand();
-			} else if (e.getSource() == jtfUneditableText) {
-				disp = "text3 : " + e.getActionCommand();
-			}
+			for(int i = 0; i < identifierValues.length; i++)
+				if(identifierValues[i] == e.getSource()) {
+					try{ 
+						values.set(i, e.getActionCommand());
+						update = true;
+						identifierValues[i].setText(names.get(i) + ": " + e.getActionCommand());
+					}catch(Exception x) {
+						JOptionPane.showMessageDialog(null, "invalid argument");
+					}
+				}
 			JOptionPane.showMessageDialog(null, disp);
 		}
 	}
-	public static void main(String args[]) {
-		RobotWhisperer test = new RobotWhisperer();
-		test.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
+}
